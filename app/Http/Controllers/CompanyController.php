e2480e5333f1cf7133e67162765f4c9273ba3af7
Iulian Cravet <iulian.webdev@gmail.com>
2019-02-18 23:20:45 +0000

    Fix test & implementation for logo-name

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -30,23 +30,15 @@ class CompanyController extends Controller
     public function index()
     {
         $allCompanies = $this->store->all();
-        return new CompaniesResource($allCompanies);
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return new CompaniesResource($allCompanies);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(CreateCompany $request)
@@ -59,26 +51,28 @@ class CompanyController extends Controller
 
         $newCompany = $this->store->create($data);
 
-        
         $img = $request->file('logo');
-        
+
         if ($img) {
             $fileName = $this->imageManager->putFile($img, $newCompany->logo_name);
-            
-            $this->store->update($newCompany->id, [
-                'logo' => $fileName
-                ]);
-                
-            $newCompany->logo = $fileName;
-            }
 
-        return new CompanyResource($newCompany);
+            $this->store->update($newCompany->id, [
+                'logo' => $fileName,
+                ]);
+
+            $newCompany->logo = $fileName;
+        }
+
+        $response = new CompanyResource($newCompany);
+
+        return $response->response()->setStatusCode(Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -89,31 +83,33 @@ class CompanyController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(CreateCompany $request, $id)
     {
-        $existingCompany =  $this->store->findOrFail($id);
-        
+        $existingCompany = $this->store->findOrFail($id);
+
         $image = $request->file('logo', '');
-        
+
         $data = $request->toArray();
-        
-        if($image) {
-            
+
+        if ($image) {
             $newFileName = $this->imageManager->putFile($image, $existingCompany->logo_name);
             $data['logo'] = $newFileName;
         } else {
             unset($data['logo']);
         }
-        
-        $update = $this->store->update($id, $data);
-        
-        if ($update) {
-            return new CompanyResource($this->store->findOrFail($id));
-            
+        $updated = $this->store->update($id, $data);
+
+        if ($updated) {
+            // dump('gets passed', $data);
+            $updatedCompany = $this->store->findOrFail($id);
+            // dd($updatedCompany);
+
+            return new CompanyResource($updatedCompany);
         }
 
         return response(Response::HTTP_BAD_REQUEST);
@@ -122,13 +118,14 @@ class CompanyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         $deletedCompany = $this->store->delete($id);
-        
+
         if ($deletedCompany->exists()) {
             if ($deletedCompany->logo) {
                 $fileErased = $this->imageManager->deleteFile($deletedCompany->logo);
@@ -136,6 +133,7 @@ class CompanyController extends Controller
                     Log::error("File not erased after company deletion. Filename:{$deletedCompany->logo}");
                 }
             }
+
             return response(Response::HTTP_OK);
         }
 
--- tests/Feature/CompanyControllerTest.php
+++ tests/Feature/CompanyControllerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+// @phpcs:disable PSR1.Methods.CamelCapsMethodName
+
 namespace Tests\Unit;
 
 use App\Company;
@@ -7,21 +9,19 @@ use App\Contracts\CompanyStore;
 use App\Employee;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
-use \Mockery;
+use Mockery;
 
 class CompanyControllerTest extends TestCase
 {
-
     use RefreshDatabase;
 
     private $repository;
 
-    public function setUp() 
+    public function setUp()
     {
         parent::setUp();
         $this->seed();
@@ -29,19 +29,19 @@ class CompanyControllerTest extends TestCase
         $this->mock = Mockery::mock(CompanyStore::class);
     }
 
-    private function withMock() 
+    private function withMock()
     {
         $this->app->instance(CompanyStore::class, $this->mock);
     }
 
-    public function tearDown() 
+    public function tearDown()
     {
         Mockery::close();
         parent::tearDown();
     }
 
     /** @test */
-    public function endpoint_not_accessible_for_guests() 
+    public function endpoint_not_accessible_for_guests()
     {
         $this->withMock();
         // one endpoint will do for now
@@ -50,11 +50,8 @@ class CompanyControllerTest extends TestCase
         $response->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
 
-
     /**
-     * @test READ endpoint for Companies 
-     *
-     * @return void
+     * @test READ endpoint for Companies
      */
     public function index_returns_all_companies()
     {
@@ -69,23 +66,22 @@ class CompanyControllerTest extends TestCase
             ->andReturn($fakeCompanies);
 
         $response = $this->json('GET', 'companies');
-        
+
         $response->assertStatus(Response::HTTP_OK);
 
         $response->assertJsonStructure([
             'data' => [
-                '*' => ['id', 'name','email','logo','website'],
+                '*' => ['id', 'name', 'email', 'logo', 'website'],
             ],
         ], $response->json());
-
     }
 
     /** @test */
-    public function show_returns_one_company() 
+    public function show_returns_one_company()
     {
         $this->withMock();
         $this->logIn();
-        
+
         $fakeCompany = factory(Company::class)->states('with_id')->make();
 
         $this->mock
@@ -97,15 +93,14 @@ class CompanyControllerTest extends TestCase
         $response = $this->json('GET', "companies/{$fakeCompany->id}");
         $response->assertStatus(Response::HTTP_OK);
         $response->assertJsonStructure([
-            'data' => ['id', 'name', 'email', 'logo', 'website']]
-        );
+            'data' => ['id', 'name', 'email', 'logo', 'website'],
+            ]);
     }
 
-    /** 
-     * @test CREATE 
-     * @return void
+    /**
+     * @test CREATE
      */
-    public function store_method_creates_new_company_with_logo() 
+    public function store_method_creates_new_company_with_logo()
     {
         $this->withMock();
         $this->withoutMiddleware();
@@ -124,22 +119,21 @@ class CompanyControllerTest extends TestCase
             ->once()
             ->andReturn($newCompany);
         $logo = "{$nameSlug}-{$newCompany->id}.png";
-        
+
         $this->mock
             ->shouldReceive('update')
             ->with($newCompany->id, ['logo' => $logo])
             ->andReturn(true);
 
         $response = $this->json('POST', 'companies/', $newCompany->toArray());
-        
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
 
         $response->assertJsonStructure([
-            'data' => ['id', 'name', 'email', 'logo', 'website']]
-        );
-        
+            'data' => ['id', 'name', 'email', 'logo', 'website'],
+        ]);
+
         $json = $response->json();
-        
 
         Storage::disk('public')->assertExists("images/{$json['data']['logo']}");
     }
@@ -150,11 +144,10 @@ class CompanyControllerTest extends TestCase
      * @testWith [{"name":"","email":"ok@email.com"}]
      *           [{"name":"Name Ok","email":"wrongemail.com"}]
      *           [{"name":"Name Ok","email":"ok@email.com","website":"wrong-url"}]
-     * 
+     *
      * @test CREATE >> fails with message
-     * @return void
      */
-    public function test_bad_format_attributes_receive_validation_error_msg(array $attrs) 
+    public function test_bad_format_attributes_receive_validation_error_msg(array $attrs)
     {
         $this->withMock();
         $this->logIn();
@@ -162,50 +155,34 @@ class CompanyControllerTest extends TestCase
         $response = $this->json('POST', 'companies', $attrs);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
-
     }
 
     /** @test */
-    public function test_update_returns_ok_status() 
+    public function test_update_returns_ok_status()
     {
-        $this->withMock();
+        // $this->withMock();
         $this->logIn();
 
-        $company = factory(Company::class)->create();
-        
+        $existingCompany = factory(Company::class)->create();
+
         Storage::fake('public');
 
         $newCompany = factory(Company::class)->make([
-            'logo' => UploadedFile::fake()->image('logo.png', 100, 100)
+            'logo' => UploadedFile::fake()->image('logo.png', 100, 100),
         ]);
 
         $employee = factory(Employee::class)->make();
-        
-        $data = $newCompany->toArray();
-        $this->mock
-        ->shouldReceive('findOrFail')
-        ->once()
-        ->andReturn($company)
-        ->shouldReceive('update')
-        ->once()
-        ->with($company->id, $data)
-        ->andReturn(true);
-        
-        $newCompany->logo = 'file';
-        $newCompany->employees = $employee->toArray();
-        $this->mock
-        ->shouldReceive('findOrFail')
-        ->once()
-        ->with($company->id)
-        ->andReturn($newCompany);
-        
-        $response = $this->json('PUT', "/companies/{$company->id}", $data);
-        // dd($response);
+
+        $response = $this->json('PUT', "/companies/{$existingCompany->id}", $newCompany->toArray());
+
+        $response->assertJsonStructure([
+            'data' => ['id', 'name', 'email', 'logo', 'website'],
+        ]);
         $response->assertStatus(Response::HTTP_OK);
     }
 
     /** @test */
-    public function test_destroy_method() 
+    public function test_destroy_method()
     {
         $this->withMock();
         $this->logIn();
@@ -213,28 +190,28 @@ class CompanyControllerTest extends TestCase
         Storage::fake('public');
 
         $company = factory(Company::class)->create([
-            'logo' => 'test.txt'
+            'logo' => 'test.txt',
         ]);
 
         Storage::disk('public')->put("images/{$company->logo}", 'Sample text');
-        
+
         $this->mock
             ->shouldReceive('delete')
             ->once()
             ->with(1)
             ->andReturn($company);
 
-        $response = $this->json('DELETE', "/companies/1");
+        $response = $this->json('DELETE', '/companies/1');
         $response->assertStatus(Response::HTTP_OK);
         Storage::disk('public')->assertMissing("images/{$company->logo}");
     }
 
     /**
      * @test
+     *
      * @throws ModelRelationNotEmptyException
-     * @return void
      */
-    public function test_that_company_whith_employees_cant_be_deleted() 
+    public function test_that_company_whith_employees_cant_be_deleted()
     {
         $this->logIn();
 
@@ -247,12 +224,10 @@ class CompanyControllerTest extends TestCase
         $response->assertStatus(Response::HTTP_BAD_REQUEST);
     }
 
-
-    private function logIn($user = null) 
+    private function logIn($user = null)
     {
         $user = $user ?? factory(User::class)->create();
 
         $this->be($user);
     }
-
 }
